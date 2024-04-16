c2ec2756bd11a1e5b3a1ed73119d977c1837b377
Giacomorue <123400315+Giacomorue@users.noreply.github.com>
2024-04-16 15:40:04 +0200

    Old auth verification

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,25 @@
-import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
+// import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+// import { getToken } from "next-auth/jwt";
 
-const secret = process.env.NEXTAUTH_SECRET;
+// const secret = process.env.NEXTAUTH_SECRET;
 
-export async function middleware(req: NextRequest, ev: NextFetchEvent) {
-  const session = await getToken({ req, secret });
+// export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+//   const session = await getToken({ req, secret });
 
-  // Definisci quali percorsi sono protetti
-  const protectedRoutes = [
-    "/admin",
-  ];
+//   // Definisci quali percorsi sono protetti
+//   const protectedRoutes = [
+//     "/admin",
+//   ];
 
-  // Controlla se il percorso richiesto è uno dei percorsi protetti
-  if (protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route))) {
-    if (!session) {
-      const url = req.nextUrl.clone();
-      url.pathname = "/sign-in"; // Reindirizza alla pagina di login
-      return NextResponse.redirect(url);
-    }
-  }
+//   // Controlla se il percorso richiesto è uno dei percorsi protetti
+//   if (protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route))) {
+//     if (!session) {
+//       const url = req.nextUrl.clone();
+//       url.pathname = "/sign-in"; // Reindirizza alla pagina di login
+//       return NextResponse.redirect(url);
+//     }
+//   }
 
-  // Permetti alla richiesta di proseguire se non richiede protezione o se la sessione è valida
-  return NextResponse.next();
-}
+//   // Permetti alla richiesta di proseguire se non richiede protezione o se la sessione è valida
+//   return NextResponse.next();
+// }
