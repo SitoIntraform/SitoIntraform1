51b4fbcc5733f655f6f9bf2c29ccf664a292e81c
Giacomorue <123400315+Giacomorue@users.noreply.github.com>
2024-04-16 20:38:39 +0200

    Try to use loader

--- app/[...link]/page.tsx
+++ app/[...link]/page.tsx
@@ -1,17 +1,53 @@
+import Loading from '@/components/Loading';
 import NavbarClient from '@/components/NavbarClient';
-// import PageViewer from '@/components/PageViewer';
 import ShowPageComponent from '@/components/ShowPageComponent';
-import prismadb from '@/lib/prismadb';
-import { SectionType } from '@/types';
-import { Page } from '@prisma/client';
+import axios from 'axios';
+import { NextResponse } from 'next/server';
 import React from 'react'
 
-async function page() {
-  return (
-    <>
-      {/* <PageViewer /> */}
-    </>
-  )
+export async function loader({ params }: { params: any }) {
+  const linkId = params.linkId;  // Assumi che il parametro dinamico sia 'linkId'
+
+  // Chiamata API per ottenere i dati basati su 'linkId'
+  const response = await axios.get(`/api/getInfo`);
+
+  // Restituisce la risposta con i dati e disabilita la cache
+  return NextResponse.json(response.data, {
+    headers: {
+      'Cache-Control': 'no-store, max-age=0'
+    }
+  });
 }
 
-export default page
+// Componente React per visualizzare i dati
+export default function PageViewer({ data }: { data: any }) {
+
+  console.log(data);
+
+  if (!data) {
+    return <Loading />
+  }
+
+  return (
+    <>
+      <NavbarClient
+        allLinks={data.links}
+        dev={false}
+        allPage={data.allPages}
+        links={data.navbar?.links || []}
+        logo={data.navbar?.logo || ""}
+        logoHeight={data.navbar?.logoHeight || 0}
+        logoWidth={data.navbar?.logoWidth || 0}
+        buttonHeight={data.navbar?.buttonHeight || 0}
+        buttonWidth={data.navbar?.buttonWidth || 0}
+        buttonLink={data.navbar?.buttonLink || ""}
+        buttonText={data.navbar?.buttonText || ""}
+      />
+      <ShowPageComponent
+        links={data.links}
+        allSections={data.allSectionType}
+        allPages={data.allPages}
+      />
+    </>
+  );
+}
