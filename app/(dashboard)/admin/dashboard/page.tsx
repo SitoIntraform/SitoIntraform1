28b353f68308b31af1acd0f910cadf0c4c8ab088
Giacomorue <123400315+Giacomorue@users.noreply.github.com>
2024-04-16 09:32:03 +0200

    Try to fix routing error 2

--- app/(dashboard)/admin/dashboard/page.tsx
+++ app/(dashboard)/admin/dashboard/page.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function DashboardPage() {
   return (
-    <>
+    <div className='pt-[80px]'>
       <h1 className='h1Desktop'>
         DashboardPage
       </h1>
-    </>
+    </div>
   )
 }
 
