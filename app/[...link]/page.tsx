23ef08d6b4f9ee9d4be84d446ce8757a927c1740
Giacomorue <123400315+Giacomorue@users.noreply.github.com>
2024-04-17 11:18:40 +0200

    Prisma implementation

--- app/[...link]/page.tsx
+++ app/[...link]/page.tsx
@@ -1,6 +1,9 @@
 import Loading from '@/components/Loading';
 import NavbarClient from '@/components/NavbarClient';
 import ShowPageComponent from '@/components/ShowPageComponent';
+import prismadb from '@/lib/prismadb';
+import { SectionType } from '@/types';
+import { Page } from '@prisma/client';
 import axios from 'axios';
 import { NextResponse } from 'next/server';
 import React, { useEffect, useState } from 'react'
@@ -15,31 +18,111 @@ async function getData() {
 
 export default async function PageViewer() {
 
-  const data = await getData();
+  // const data = await getData();
 
-  if (!data) {
-    return <Loading />
-  }
+  // if (!data) {
+  //   return <Loading />
+  // }
+
+  const pages: Page[] = await prismadb.page.findMany({});
+  const allSection = await prismadb.section.findMany({});
+  const navbar = await prismadb.navbar.findFirst({});
+  const links = await prismadb.link.findMany({});
+
+  const allPages = pages && [
+    ...pages.map((page) => {
+      const createdAt = new Date(page.createdAt);
+      const updatedAt = new Date(page.updatedAt);
+
+      return {
+        PageId: page.PageId || "",
+        createdAt: `${createdAt.getUTCDate()}/${createdAt.getUTCMonth() + 1
+          }/${createdAt.getFullYear()}`,
+        updatedAt: `${updatedAt.getUTCDate()}/${updatedAt.getUTCMonth() + 1
+          }/${updatedAt.getFullYear()}`,
+
+        name: page.name || "",
+        link: page.link || "",
+
+        defaltPage: page.defaltPage || false,
+
+        numberSections: page.numberSections || 0,
+        sections: page.sections || [],
+      };
+    }),
+  ];
+
+  const allSectionType: SectionType[] = allSection && [
+    ...allSection.map((sectionSingle: any) => {
+      return {
+        SectionId: sectionSingle?.SectionId || "",
+        updatedAt: sectionSingle?.updatedAt.toString() || "",
+        createdAt: sectionSingle?.createdAt.toString() || "",
+        name: sectionSingle?.name || "",
+        pageType: sectionSingle?.pageType || "Hero",
+
+        data: {
+          animation: sectionSingle?.data.animation || false,
+          animationType: sectionSingle?.data.animationType || "up",
+
+          backgroundImages: sectionSingle?.data.backgroundImages || "",
+          backgroundImageOpacity:
+            sectionSingle?.data.backgroundImageOpacity || 100,
+          backgroundColor: sectionSingle?.data.backgroundColor || "",
+
+          images: sectionSingle?.data.images || [],
+          imagesOnLeft: sectionSingle?.data.imagesOnLeft || true,
+
+          textBlue: sectionSingle?.data.textBlue || "",
+          textGreen: sectionSingle?.data.textGreen || "",
+          textBlack: sectionSingle?.data.textBlack || "",
+          description: sectionSingle?.data.description || "",
+
+          carouselDots: sectionSingle?.data.carouselDots || false,
+          carouselButtons: sectionSingle?.data.carouselButtons || false,
+
+          service: sectionSingle?.data.service || [],
+
+          hScreen: sectionSingle?.data.hScreen || false,
+          space: sectionSingle?.data.space || 40,
+
+          primaryButton: sectionSingle?.data.primaryButton || false,
+          primaryButtonText: sectionSingle?.data.primaryButtonText || "",
+          primaryLink: sectionSingle?.data.primaryLink || "",
+          heightPrimaryButton: sectionSingle?.data.heightPrimaryButton || 0,
+          widthPrimaryButton: sectionSingle?.data.widthPrimaryButton || 0,
+
+          secondaryButton: sectionSingle?.data.secondaryButton || false,
+          secondaryButtonText: sectionSingle?.data.secondaryButtonText || "",
+          secondaryLink: sectionSingle?.data.secondaryLink || "",
+          heightSecondaryButton: sectionSingle?.data.heightSecondaryButton || 0,
+          widthSecondaryButton: sectionSingle?.data.widthSecondaryButton || 0,
+
+          faq: sectionSingle?.data.faq || [],
+        },
+      };
+    }),
+  ];
 
   return (
     <>
       <NavbarClient
-        allLinks={data.links}
+        allLinks={links}
         dev={false}
-        allPage={data.allPages}
-        links={data.navbar?.links || []}
-        logo={data.navbar?.logo || ""}
-        logoHeight={data.navbar?.logoHeight || 0}
-        logoWidth={data.navbar?.logoWidth || 0}
-        buttonHeight={data.navbar?.buttonHeight || 0}
-        buttonWidth={data.navbar?.buttonWidth || 0}
-        buttonLink={data.navbar?.buttonLink || ""}
-        buttonText={data.navbar?.buttonText || ""}
+        allPage={allPages}
+        links={navbar?.links || []}
+        logo={navbar?.logo || ""}
+        logoHeight={navbar?.logoHeight || 0}
+        logoWidth={navbar?.logoWidth || 0}
+        buttonHeight={navbar?.buttonHeight || 0}
+        buttonWidth={navbar?.buttonWidth || 0}
+        buttonLink={navbar?.buttonLink || ""}
+        buttonText={navbar?.buttonText || ""}
       />
       <ShowPageComponent
-        links={data.links}
-        allSections={data.allSectionType}
-        allPages={data.allPages}
+        links={links}
+        allSections={allSectionType}
+        allPages={allPages}
       />
     </>
   );
