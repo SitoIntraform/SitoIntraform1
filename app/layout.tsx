4233169f2b1ede3e44a356952315944b2f8738b4
Giacomorue <123400315+Giacomorue@users.noreply.github.com>
2024-04-19 18:46:29 +0200

    Meta tag to hide the url bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-capable" content="yes"></meta>
+      </head>
       <body className={`${sans.className} ${roboto.className} !w-screen !max-w-[100vw] !overflow-x-hidden h-[100svh] max-h-[100svh] min-h-[100svh] overflow-y-scroll`}>
         <ToastProvider />
         {children}
