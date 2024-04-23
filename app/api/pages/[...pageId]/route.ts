3f3f88d3157542d55c9f01e77c6cd2b41b75b922
Giacomorue <123400315+Giacomorue@users.noreply.github.com>
2024-04-23 19:27:12 +0200

    Add dbobjectId

--- app/api/pages/[...pageId]/route.ts
+++ app/api/pages/[...pageId]/route.ts
@@ -87,64 +87,66 @@ export async function POST(
     }
 
     //SISTEMARE IL COLLEGAMENTO PAGINA SEZIONE
-    await prismadb.section.updateMany({
-      where: {
-        PageId: pageId[0].toString(),
-      },
-      data: {
-        PageId: null,
-      },
-    });
-
-    sections.map(async (s: Section) => {
-      await prismadb.section.update({
+    await prismadb.section
+      .updateMany({
         where: {
-          SectionId: s.SectionId,
+          PageId: pageId[0],
         },
         data: {
-          PageId: pageId[0].toString(),
-          data: {
-            animation: s.data.animation,
-            animationType: s.data.animationType,
-
-            backgroundImages: s.data.backgroundImages,
-            backgroundImageOpacity: s.data.backgroundImageOpacity,
-            backgroundColor: s.data.backgroundColor,
-
-            images: s.data.images,
-            imagesOnLeft: s.data.imagesOnLeft,
-
-            textBlue: s.data.textBlue,
-            textGreen: s.data.textGreen,
-            textBlack: s.data.textBlack,
-            description: s.data.description,
-
-            carouselDots: s.data.carouselDots,
-            carouselButtons: s.data.carouselButtons,
-
-            service: s.data.service,
-
-            hScreen: s.data.hScreen,
-            space: s.data.space,
-
-            primaryButton: s.data.primaryButton,
-            primaryButtonText: s.data.primaryButtonText,
-            primaryLink: s.data.primaryLink,
-            widthPrimaryButton: s.data.widthPrimaryButton,
-            heightPrimaryButton: s.data.heightPrimaryButton,
-
-            secondaryButton: s.data.secondaryButton,
-            secondaryButtonText: s.data.secondaryButtonText,
-            secondaryLink: s.data.secondaryLink,
-            widthSecondaryButton: s.data.widthSecondaryButton,
-            heightSecondaryButton: s.data.heightSecondaryButton,
-
-            faq: s.data.faq,
-            courseId: s.data.courseId,
-          },
+          PageId: null,
         },
+      })
+      .then(() => {
+        sections.map(async (s: Section) => {
+          await prismadb.section.update({
+            where: {
+              SectionId: s.SectionId,
+            },
+            data: {
+              PageId: pageId[0],
+              data: {
+                animation: s.data.animation,
+                animationType: s.data.animationType,
+
+                backgroundImages: s.data.backgroundImages,
+                backgroundImageOpacity: s.data.backgroundImageOpacity,
+                backgroundColor: s.data.backgroundColor,
+
+                images: s.data.images,
+                imagesOnLeft: s.data.imagesOnLeft,
+
+                textBlue: s.data.textBlue,
+                textGreen: s.data.textGreen,
+                textBlack: s.data.textBlack,
+                description: s.data.description,
+
+                carouselDots: s.data.carouselDots,
+                carouselButtons: s.data.carouselButtons,
+
+                service: s.data.service,
+
+                hScreen: s.data.hScreen,
+                space: s.data.space,
+
+                primaryButton: s.data.primaryButton,
+                primaryButtonText: s.data.primaryButtonText,
+                primaryLink: s.data.primaryLink,
+                widthPrimaryButton: s.data.widthPrimaryButton,
+                heightPrimaryButton: s.data.heightPrimaryButton,
+
+                secondaryButton: s.data.secondaryButton,
+                secondaryButtonText: s.data.secondaryButtonText,
+                secondaryLink: s.data.secondaryLink,
+                widthSecondaryButton: s.data.widthSecondaryButton,
+                heightSecondaryButton: s.data.heightSecondaryButton,
+
+                faq: s.data.faq,
+                courseId: s.data.courseId,
+              },
+            },
+          });
+        });
       });
-    });
 
     const page = await prismadb.page.update({
       data: {
